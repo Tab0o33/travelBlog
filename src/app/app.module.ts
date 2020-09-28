bf681faae598aa397fc1e4dd460cee195af75a3d
Tab0o33 <w.moinet@orange.fr>
2020-09-28 18:27:32 +0200

    feat(sidebar): rename component from navbar to sidebar + ajout des items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
+    SidebarComponent,
     ContainerComponent
   ],
   imports: [
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,16 +2,23 @@ import { Input } from '@angular/core';
 import { Component, Output, EventEmitter, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
 })
-export class NavbarComponent {
+export class SidebarComponent {
 
   @ViewChild('sidebar') sidebar: ElementRef;
 
   @Input() sideBarOpen: boolean;
   @Output() toggleSideBar: EventEmitter<any> = new EventEmitter();
+
+  items = [
+    { icon: 'home', label: 'Home' },
+    { icon: 'menu_book', label: 'Sommaire' },
+    { icon: 'public', label: 'Pays' },
+    { icon: 'photo', label: 'Galerie' }
+  ];
 
   constructor() { }
 
